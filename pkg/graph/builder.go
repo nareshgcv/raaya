c722983479f8188eaaa6537c8ceff76812a5c728
nareshgcv <nareshgv145@gmail.com>
2026-09-21 19:17:01 +0530

    Implement Builder for SecurityGraph management

--- pkg/graph/builder.go
+++ pkg/graph/builder.go
@@ -1,1 +1,25 @@
+package graph
 
+type Builder struct {
+	graph *SecurityGraph
+}
+
+func NewBuilder() *Builder {
+	return &Builder{
+		graph: NewSecurityGraph(),
+	}
+}
+
+func (b *Builder) Graph() *SecurityGraph {
+	return b.graph
+}
+
+func (b *Builder) LinkServerToTool(serverID string, toolID string) {
+	edge := &Edge{
+		ID:       serverID + "->" + toolID,
+		SourceID: serverID,
+		TargetID: toolID,
+		Type:     EdgeHosts,
+	}
+	b.graph.AddEdge(edge)
+}
