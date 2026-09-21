8545752a20de22487ea134ddaea2544727fa9cec
nareshgcv <nareshgv145@gmail.com>
2026-09-21 19:45:10 +0530

    Add terminal rendering for security graph
    Implement terminal rendering for the APE Dependency Graph.

---
+++ pkg/reporter/terminal.go
@@ -0,0 +1,32 @@
+package reporter
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ape-sec/ape/pkg/graph"
+)
+
+func RenderTerminalTree(sg *graph.SecurityGraph) {
+	fmt.Println("\n=== APE Dependency Graph ===")
+
+	for _, node := range sg.Nodes {
+		if node.Type == graph.NodeAgent || node.Type == graph.NodeServer {
+			fmt.Printf("● [%s] %s\n", node.Type, node.Name)
+			renderOutgoingEdges(sg, node.ID, "  ")
+		}
+	}
+	fmt.Println()
+}
+
+func renderOutgoingEdges(sg *graph.SecurityGraph, nodeID string, indent string) {
+	for _, edge := range sg.Edges {
+		if edge.SourceID == nodeID {
+			target := sg.Nodes[edge.TargetID]
+			if target != nil {
+				fmt.Printf("%s└── (%s) ──> [%s] %s\n", indent, edge.Type, target.Type, target.Name)
+				renderOutgoingEdges(sg, target.ID, indent+"    ")
+			}
+		}
+	}
+}
