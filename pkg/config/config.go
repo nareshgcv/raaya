d182da04af8fa919f57b3378286c3f4d23fdc0d0
nareshgcv <nareshgv145@gmail.com>
2026-09-21 19:38:09 +0530

    Implement configuration loading from YAML files
    Added configuration structures and LoadConfig function to handle YAML configuration files.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,41 @@
+package config
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	IgnorePaths []string          `yaml:"ignore_paths"`
+	Thresholds  ThresholdSettings `yaml:"thresholds"`
+	Rules       RuleSettings     `yaml:"rules"`
+}
+
+type ThresholdSettings struct {
+	MaxBlastRadius int `yaml:"max_blast_radius"`
+}
+
+type RuleSettings struct {
+	Disabled []string `yaml:"disabled"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return &Config{
+			Thresholds: ThresholdSettings{MaxBlastRadius: 5},
+		}, nil
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
