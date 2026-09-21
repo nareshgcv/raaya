6e019a2eb2eb6b33b8ca7c3d38a2b7e310881f20
nareshgcv <nareshgv145@gmail.com>
2026-09-21 19:25:34 +0530

    Implement transitive reach calculation in calculator.go

--- pkg/blastradius/calculator.go
+++ pkg/blastradius/calculator.go
@@ -1,1 +1,24 @@
+package blastradius
 
+import "github.com/raaya/pkg/graph"
+
+type RadiusMap map[string][]string
+
+func CalculateTransitiveReach(sg *graph.SecurityGraph, startNodeID string) []string {
+	visited := make(map[string]bool)
+	reachable := make([]string, 0)
+
+	var dfs func(currID string)
+	dfs = func(currID string) {
+		visited[currID] = true
+		for _, edge := range sg.Edges {
+			if edge.SourceID == currID && !visited[edge.TargetID] {
+				reachable = append(reachable, edge.TargetID)
+				dfs(edge.TargetID)
+			}
+		}
+	}
+
+	dfs(startNodeID)
+	return reachable
+}
