3c09aed25e1840a9a8b148022efe22b9d4a88a31
nareshgcv <nareshgv145@gmail.com>
2026-09-21 19:41:48 +0530

    Add ExportToDOT function for SecurityGraph
    Implement ExportToDOT function to convert SecurityGraph to DOT format.

--- pkg/graph/export.go
+++ pkg/graph/export.go
@@ -1,1 +1,25 @@
+package graph
 
+import (
+	"fmt"
+	"strings"
+)
+
+func ExportToDOT(sg *SecurityGraph) string {
+	var sb strings.Builder
+	sb.WriteString("digraph SecurityGraph {\n")
+	sb.WriteString("  rankdir=LR;\n")
+	sb.WriteString("  node [shape=box, style=filled, color=lightgrey];\n\n")
+
+	for _, node := range sg.Nodes {
+		sb.WriteString(fmt.Sprintf("  \"%s\" [label=\"%s\\n(%s)\"];\n", node.ID, node.Name, node.Type))
+	}
+
+	sb.WriteString("\n")
+	for _, edge := range sg.Edges {
+		sb.WriteString(fmt.Sprintf("  \"%s\" -> \"%s\" [label=\"%s\"];\n", edge.SourceID, edge.TargetID, edge.Type))
+	}
+
+	sb.WriteString("}\n")
+	return sb.String()
+}
